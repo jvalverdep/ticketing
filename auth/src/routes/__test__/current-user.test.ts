8e2da72de42f865228db85f572fa6b86f9307dad
Javier Valverde <javier.valverde.pa@gmail.com>
2021-09-17 19:34:51 -0500

    Section 10: Testing Isolated Microservices

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import app from "../../app";
+import { signin } from "../../test/helpers";
+
+it("responds with details about the current user", async () => {
+  const cookie = await signin();
+
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual("test@test.com");
+});
