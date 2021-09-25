7e85dea15266ac680d846380640e81dd5b2844ba
Javier Valverde <javier.valverde.pa@gmail.com>
2021-09-24 21:48:34 -0500

    using @jvptickets/common

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,11 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 
-import { errorHandler } from "./middlewares/error-handler";
+import { errorHandler, NotFoundError } from "@jvptickets/common";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
-
-import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.set("trust proxy", true);
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,8 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { BadRequestError } from "../errors/bad-request-error";
-import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError, validateRequest } from "@jvptickets/common";
 import { PasswordManager } from "../services/password-manager";
 import { User } from "../models/user";
 
